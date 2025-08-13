e9106ecd6503da890572f94f0e9be178e2ea0955
Ckemplen <87258247+Ckemplen@users.noreply.github.com>
2025-08-13 17:52:17 +0100

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class)->in('Feature');
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
